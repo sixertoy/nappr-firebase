e0b3015541930cb4a621bc5eb033513e515bd7c0
matthieu <sixertoy.github@gmail.com>
2021-09-22 16:53:38 +0200

    firebase v9
    - refacto all the things \o/

--- src/core/index.js
+++ src/core/index.js
@@ -1,4 +1,3 @@
+export { default as getConfig } from './get-config';
+export { default as initialize } from './initialize';
 export { default as renderer } from './renderer';
-export { default as getFirebaseConfig } from './get-firebase-config';
-export { default as FirebaseAuthContext } from './firebase-auth-context';
-export { default as initFirebaseWithConfig } from './init-firebase-with-config';
