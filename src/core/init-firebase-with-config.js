f917793cd97c2dfd99614d42bcb1a34206dcf913
matthieu <sixertoy.github@gmail.com>
2021-09-22 22:28:02 +0200

    ah ah ah

--- src/core/init-firebase-with-config.js
+++ src/core/init-firebase-with-config.js
@@ -5,12 +5,11 @@ import { FIREBASE_DEFAULT_APPNAME } from '../constants';
 function initFirebaseWithConfig(config, name = FIREBASE_DEFAULT_APPNAME) {
   let app = null;
   try {
-    app = getApp(name);
+    app = initializeApp(config, name);
   } catch (err) {
     if (err.code !== 'app/duplicate-app') {
       throw err;
     }
-    app = initializeApp(config, name);
   }
   return app;
 }
