1cca305ed9c106c4067fbb1ba38b1d513a771af3
matthieu <sixertoy.github@gmail.com>
2021-09-22 22:21:46 +0200

    fix try/catch getApp

--- src/core/init-firebase-with-config.js
+++ src/core/init-firebase-with-config.js
@@ -3,15 +3,14 @@ import { getApp, initializeApp } from 'firebase/app';
 import { FIREBASE_DEFAULT_APPNAME } from '../constants';
 
 function initFirebaseWithConfig(config, name = FIREBASE_DEFAULT_APPNAME) {
-  let app = getApp(name);
-  if (app) return app;
-
+  let app = null;
   try {
-    app = initializeApp(config, name);
+    app = getApp(name);
   } catch (err) {
     if (err.code !== 'app/duplicate-app') {
       throw err;
     }
+    app = initializeApp(config, name);
   }
   return app;
 }
